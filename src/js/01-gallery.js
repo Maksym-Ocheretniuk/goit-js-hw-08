79b250e4d9de05e67fd87c7770a36accfaf960d9
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-03-25 16:58:34 +0200

    task 08-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Знаходимо галерею та її елементи "li"
+const galleryEl = document.querySelector('.gallery');
+
+// Додаємо розмітку в структуру
+galleryEl.innerHTML = createListImg(galleryItems);
+
+// Функція створення розмітки "li" списку в HTML
+function createListImg(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+// бібліотека SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
